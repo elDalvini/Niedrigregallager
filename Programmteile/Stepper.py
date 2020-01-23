518c16f0d5c15633687a681ceeed4e828a6268fd
David Monninger <david@monninger.de>
2020-01-23 18:59:03 +0100

    commentary

--- Programmteile/Stepper.py
+++ Programmteile/Stepper.py
@@ -1,10 +1,3 @@
-
-#from RpiMotorLib import RpiMotorLib
-
-#motor = RpiMotorLib.BYJMotor("a","nema")
-
-#motor.motor_run([19,21,26,13],0.001, 5000,0,0,"full",0)     #([pins], delay, steps, ccwise, verbose, "stepType", initDelay)
-
 import RPi.GPIO as GPIO
 import time
 import threading
@@ -14,57 +7,71 @@ GPIO.setmode(GPIO.BCM)
 
 
 class Stepper:
-    steps = 0
-    StepPin = 0
-    DirPin = 0
-    EndPin = 0
-    Delay = 0
+    steps = 0       #current Position
+    StepPin = 0     #GPIO connected to 'STEP' Pin of A4988 driver
+    DirPin = 0      #GPIO connected to 'DIR' Pin of A4988 driver
+    EndPin = -1     #GPIO connected to optional end switch (normally closed), -1 if none connected  
+    Delay = 0       #Delay between steps (in seconds)
 
-
+    #Initialisation:
     def __init__(self, Step, Dir, StepDelay, End = -1):
+        #store set Values of the Motor:
         self.steps = 0
         self.StepPin = Step
         self.DirPin = Dir
         self.Delay = StepDelay
         self.EndPin = End
+
+        #Setup GPIOs
         GPIO.setup(self.StepPin, GPIO.OUT)
         GPIO.setup(self.DirPin, GPIO.OUT)
-        if self.EndPin != -1:
+        if self.EndPin != -1:   #only setup GPIO or Endswitch if one is connected
             GPIO.setup(self.EndPin, GPIO.IN)
 
+    #Move a defined number of steps
     def Step(self, steps, dir):
-        GPIO.output(self.DirPin, dir)
+        GPIO.output(self.DirPin, dir)       #set direction
+
+        #pulse STEP output for each step
         for i in range(steps):
             GPIO.output(self.StepPin,1)
             time.sleep(self.Delay/2)
             GPIO.output(self.StepPin,0)
             time.sleep(self.Delay/2)
 
-            self.steps += (dir*2-1)
+            self.steps += (dir*2-1)         #increment (or decrement) current position
 
+    #Move to a defined Position
     def Move(self, pos):
-        if pos != self.steps:
-            dir = not (pos < self.steps)
-            self.Step(abs(self.steps - pos), dir)
+        if pos != self.steps:       #do nothing if the motor is already at the defined position 
+            dir = not (pos < self.steps)            #set direction
+            self.Step(abs(self.steps - pos), dir)   #move the difference between current and determined position
 
+    #Move in the given direction until the end switch is pressed
     def Home(self,dir):
-        if self.EndPin != -1:
+        if self.EndPin != -1:   #do nothing if no end switch is connected
+            #move one step at a time until the end switch is pressed
             while GPIO.input(self.EndPin):
                 self.Step(1,dir)
 
-            self.steps = 0
+            self.steps = 0  #set current position to zero
 
         
-
+#Stepper Object definition
 mX = Stepper(19, 26, 0.005, 5)
 mY = Stepper(6, 13, 0.005)
 
-mX.Step(150,1)
-time.sleep(0.25)
-mX.Move(75)
-time.sleep(0.5)
-mX.Home(0)
+###########Move and Home example#####
+#
+#mX.Step(150,1)
+#time.sleep(0.25)
+#mX.Move(75)
+#time.sleep(0.5)
+#mX.Home(0)
 
+
+############Threading example########
+#
 #MovX = threading.Thread(target = mX.Step , args=(500,1))
 #MovY = threading.Thread(target = mY.Step, args = (100,0))
 #MovX.start()
@@ -76,6 +83,8 @@ mX.Home(0)
 #print("X done")
 
 
+##########Step example###############
+#
 #mX.Step(50,1)
 #print(str(mX.steps))
 #time.sleep(0.25)
