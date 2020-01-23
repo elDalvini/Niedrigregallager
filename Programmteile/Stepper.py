b1961bb52ea972b9c34df09cc5aa86d4430ee262
David Monninger <david@monninger.de>
2020-01-23 18:07:08 +0100

    threading test

--- Programmteile/Stepper.py
+++ Programmteile/Stepper.py
@@ -7,6 +7,7 @@
 
 import RPi.GPIO as GPIO
 import time
+import threading
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -39,13 +40,24 @@ class Stepper:
 mX = Stepper(19, 26, 0.005)
 mY = Stepper(6, 13, 0.005)
 
-mX.Step(50,1)
-print(str(mX.steps))
-time.sleep(0.25)
-mY.Step(50,1)
-print(str(mX.steps))
-time.sleep(0.25)
-mX.Step(25,0)
-print(str(mX.steps))
+MovX = threading.Thread(target = mX.Step , args=(500,1))
+MovY = threading.Thread(target = mY.Step, args = (100,0))
+MovX.start()
+time.sleep(1)
+MovY.start()
+MovY.join()
+print("Y done")
+MovX.join()
+print("X done")
+
+
+#mX.Step(50,1)
+#print(str(mX.steps))
+#time.sleep(0.25)
+#mY.Step(50,1)
+#print(str(mX.steps))
+#time.sleep(0.25)
+#mX.Step(25,0)
+#print(str(mX.steps))
 
 
